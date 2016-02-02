c40d2ded556bec66e85565e76a8e6b3ee8598060
Ben Cordero (bencord0) <bencord0@condi.me>
2016-02-02 23:31:29 +0000

    Allow multiple files to be uploaded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var multer = require('multer');
 var upload = multer({dest: 'uploads/'});
@@ -7,14 +8,16 @@ var app = express();
 function log_error(err) {
     if (err != null) {
         console.log(err);
+        return true;
     }
+    return false;
 };
 
-app.use(express.static('static'));
-app.post('/', upload.single('uploadedfile'), function(req, res, next) {
 
-    var original = req.file.destination + '/' + req.file.originalname;
-    var mangled = req.file.destination + '/' + req.file.filename;
+function handle_file(file) {
+    var original = path.join(file.destination, file.originalname);
+    var mangled = path.join(file.destination, file.filename);
+
     fs.stat(original, function (err, stats) {
         if (!stats) {
             fs.rename(mangled, original, log_error);
@@ -22,8 +25,23 @@ app.post('/', upload.single('uploadedfile'), function(req, res, next) {
             fs.unlink(mangled, log_error);
         };
     });
+}
 
-   res.redirect('/');
+
+app.use(express.static('static'));
+app.post('/', function(req, res) {
+
+  uploader = upload.array('uploadedfile');
+  uploader(req, res, function (err) {
+    if (log_error(err)) return;
+
+    // everything is fine
+    for (file in req.files) {
+      handle_file(req.files[file])
+    }
+
+    res.redirect('/');
+  });
 
 });
 

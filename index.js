65d381686ccbec25df829890c58e363c8c531459
Ben Cordero <becorder@cisco.com>
2016-02-02 17:15:44 +0000

    Handle repeated files (by dropping them)

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ var app = express();
 
 app.use(express.static('static'));
 app.post('/', upload.single('uploadedfile'), function(req, res, next) {
-    fs.rename(req.file.destination + '/' + req.file.filename,
-              req.file.destination + '/' + req.file.originalname,
-              function (err) {
-                  if (err != null) {
-                      console.log(err);
-                  }
-              })
-    res.end();
+
+    var original = req.file.destination + '/' + req.file.originalname;
+    var mangled = req.file.destination + '/' + req.file.filename;
+    fs.stat(original, function (err, stats) {
+        if (!stats) {
+            fs.rename(mangled, original, function (err) {
+                if (err != null) {
+                    console.log(err);
+                }
+            });
+        } else {
+            fs.unlink(mangled, function (err) {
+                if (err != null) {
+                    console.log(err);
+                }
+            });
+        };
+    });
+
+   res.redirect('/');
 
 });
 
